97cdecb83f0ca104a247683824d74deb816a1375
Nathan - Lithia <nathanlol5@live.com.au>
2021-07-23 20:18:21 +0930

    default disable shadows
    its easier on the FPS for lower end PC's

--- shaders/lib/common.glsl
+++ shaders/lib/common.glsl
@@ -187,7 +187,7 @@ Complementary Shaders by EminGT, based on BSL Shaders by Capt Tatsu
   const float sunPathRotation = -40.0; //[-60.0 -55.0 -50.0 -45.0 -40.0 -35.0 -30.0 -25.0 -20.0 -15.0 -10.0 -5.0 0.0 5.0 10.0 15.0 20.0 25.0 30.0 35.0 40.0 45.0 50.0 55.0 60.0]
   const int shadowMapResolution = 1024; //[256 1024 2048 3072 4096 8192]
   const float shadowDistance = 128.0; //[64.0 96.0 128.0 192.0 256.0 384.0 512.0 768.0 1024.0]
-  #define SHADOWS
+//#define SHADOWS
 //#define SHADOW_FILTER
   #define SHADOW_SUBSURFACE 2 //[0 1 2]
   #define SCATTERING_LEAVES 0.50 //[0.00 0.05 0.10 0.15 0.20 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 0.85 0.90 0.95 1.00]
